612de6534f655ffa418e47b0a24e547d87ab343e
Tyler Duffus <tyled94@hotmail.com>
2024-09-01 11:55:15 +1000

    Added support for Luctus Medical
    refs #6

--- lua/attachment_vendor/server/sv_attachment_vendor_override.lua
+++ lua/attachment_vendor/server/sv_attachment_vendor_override.lua
@@ -50,4 +50,59 @@ hook.Add("Initialize", "AttachmentVendorOverride", function()
          ply:removeWeaponAttachments(att, amt);
       end
    end
+
+   -- Luctus Medic revive
+   if (istable(LUCTUS_MEDIC_HITGROUPS)) then -- Luctus Medical has individual globals for each config so check if an arbitrary global exists
+      ATTACHMENT_VENDOR.override.LuctusMedicReturnWeapons = LuctusMedicReturnWeapons;
+      function LuctusMedicReturnWeapons(ply)
+         ATTACHMENT_VENDOR.override.LuctusMedicReturnWeapons(ply)
+
+         -- No death attachments so do nothing
+         if (not istable(ply.deathAttachments) or #ply.deathAttachments == 0) then return end
+
+         -- give normal weapon attachments
+         ply:giveWeaponAttachments(ply.deathAttachments, false, false); -- The first `false` makes it not notify the player that they received the attachments. The second will force these attachments to be networked
+
+         -- give cw2.0 mags
+         for mag, count in pairs(ply.deathCW2Mags) do
+            ply:giveWeaponAttachments(mag, count, false) -- `false` will force these to be networked
+         end
+
+         ply.deathAttachments = nil
+         ply.deathCW2Mags = nil
+      end
+
+      hook.Add("PlayerDeath","AttVend:LuctusMedicAttachments",function (ply)
+         local attDict = {}; -- attachment name : true pairs to make sure we dont double up on attachments. `table.HasValue` might be nicer but this is significantly quicker
+         ply.deathAttachments = {} -- attachment name values
+         ply.deathCW2Mags = {} -- mag name : count pairs
+
+         -- CW2.0
+         for att,_ in pairs(ply.CWAttachments or {}) do
+            if (not attDict[att]) then
+               table.insert(ply.deathAttachments, att)
+            end
+         end
+
+         -- CW2.0 Mag System
+         for mag, count in pairs(ply.cwMagazines or {}) do
+            ply.deathCW2Mags[mag] = ply.deathCW2Mags[mag] or 0;
+            ply.deathCW2Mags[mag] = ply.deathCW2Mags[mag] + count
+         end
+
+         -- FAS2
+         for _, att in pairs(ply.FAS2Attachments or {}) do
+            if (not attDict[att]) then
+               table.insert(ply.deathAttachments, att)
+            end
+         end
+
+         -- ArcCW
+         for att, _ in pairs(ply.ArcCW_AttInv or {}) do
+            if (not attDict[att]) then
+               table.insert(ply.deathAttachments, att)
+            end
+         end
+      end)
+   end
 end);
